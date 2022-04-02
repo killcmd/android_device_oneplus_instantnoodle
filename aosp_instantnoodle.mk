1c1e09ad9078a6184edcf269a0abdea07600072d
Ferdinand Vasquez <killcmd@gmail.com>
2022-04-02 17:04:50 -0400

    Updated Variables

--- aosp_instantnoodle.mk
+++ aosp_instantnoodle.mk
@@ -15,6 +15,9 @@
 # limitations under the License.
 #
 
+# Snack Packs Options
+SNACK := AEX
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
